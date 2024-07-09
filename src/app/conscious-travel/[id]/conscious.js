e80f6ef313404459c1f7cccc3a8a041ecf2fb4d8
Priyesh Rai <105197078+PriyeshRai369@users.noreply.github.com>
2024-07-09 16:59:17 +0530

    some ui changes done

--- src/app/conscious-travel/[id]/conscious.js
+++ src/app/conscious-travel/[id]/conscious.js
@@ -7,13 +7,23 @@ const ConsciousDetail = ({ dest }) => {
   const id = dest
   const [data, setData] = useState({})
   const [loading, setLoading] = useState(true);
-  console.log(data.conscious_content);
+  // console.log(data);
+  const [packages,setPackages]= useState([])
+
+  useEffect(() => {
+    if (data && data.with_package) {
+      setPackages(data.with_package)
+    }
+  }, [data]);
+
+  // console.log(packages);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(`https://www.banarasialoopapad.in/api-conscious-travel/${id}`);
         setData(response.data[0]);
-        // console.log(response.data[0]);
+
       } catch (error) {
         console.error("Error in Fetching Data", error);
       } finally {
@@ -50,12 +60,30 @@ const ConsciousDetail = ({ dest }) => {
           <div className="container">
             <div className="row">
               <div className="col-xl-12 col-lg-12 text-justify travel_con_de">
-                <h2>{data.customer_name}</h2>
-                {data && data.conscious_content && data.conscious_content.map((content) => (
+                <h1 className='headingTitle display-4'>{data.title}</h1>
+                <div className='logoAndHeading'>
+                  <div className='img-Container'>
+                    <img src={data.main_banner} alt="" />
+                  </div>
+                  <div>
+                    <h2 style={{ marginBottom: "-10px" }} dangerouslySetInnerHTML={{ __html: data.author }}></h2>
+                    <p style={{ margin: "0" }}>{data.customer_name}</p>
+                  </div>
+                </div>
+              </div>
+        
+                <div >
+
+                  {packages.map((items)=>(
+                    <a class="post-category__btn trevlo-btn trevlo-btn--base" href={`/packages/${items.package_url}`}  style={{padding:" 6px 5px",fontSize:"12px",margin:"0 5px"}}><span>{items.package_name}</span></a>
+                  ))}
+                </div>
+              {data && data.conscious_content && data.conscious_content.map((content) => (
+                <div className="col-xl-12 col-lg-12 text-justify travel_con_de">
                   <div className="shadowBox" key={content.conscious_id}>
                     {content.conscious_img && (
                       <div className="col-md-4 rightsidediv">
-                        <img src={content.conscious_img} alt={content.blog_heading} className='consimg rounded float-left' />
+                        <img src={content.conscious_img} alt={content.blog_heading} className='consimg1 w-100 rounded float-left' />
                       </div>
                     )}
                     <div className="tour-listing-details__reviews-content wow animated fadeInUp animated"
@@ -65,8 +93,9 @@ const ConsciousDetail = ({ dest }) => {
                       <p className="tour-listing-details__reviews-text" dangerouslySetInnerHTML={{ __html: content.conscious_content }}></p>
                     </div>
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
+
             </div>
           </div>
         </section>}
