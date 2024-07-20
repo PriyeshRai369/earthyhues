6fce4aded2a8f5eaa5be41230255796618ed3392
Priyesh Rai <105197078+PriyeshRai369@users.noreply.github.com>
2024-07-20 13:23:19 +0530

    add clear text box functionality.

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -89,8 +89,10 @@ function Blog() {
                   Travel Blog
                 </span>
                 <div className='searchSection'>
-                  <input type="text" placeholder='Search.......' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={{ fill: "#13a5b6" }}><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
+                  <input type="text" placeholder='Search.......' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+                  {searchQuery.length ? 
+                  <span className='clearText' onClick={()=>setSearchQuery("")}>x</span>:""}
                 </div>
               </h3>
               <div className="row">
