85542536cd5164c3b79d7a15bab2607a01333c35
PriyeshRai369 <priyeshrai369@gmail.com>
2024-08-01 13:01:30 +0530

    new update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     siteUrl: 'https://earthyhues-sage.vercel.app/',
     generateRobotsTxt: true,
+    transform: async (config, path) => {
+        return {
+          loc: path, 
+          changefreq: 'daily',
+          priority: 0.7,
+          lastmod: new Date().toISOString(),
+        };
+      },
   };
